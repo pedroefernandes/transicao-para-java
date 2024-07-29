56f903a73ed2f810d8e59faa8956d7c5874db7ec
Pedro Ernesto <ernestopedro@edu.unifor.br>
2024-07-29 20:13:10 -0300

    Update Account.java

--- TransicaoJava/src/Parte01/ClassesObjetos/AccountSystem/Account.java
+++ TransicaoJava/src/Parte01/ClassesObjetos/AccountSystem/Account.java
@@ -1,31 +1,31 @@
 package Parte01.ClassesObjetos.AccountSystem;
 
 public class Account {
-  private String accountHolder;
-  private double balance;
+    String accountHolder;
+    double balance;
 
-  public Account(String accountHolder, double initialBalance) {
-      this.accountHolder = accountHolder;
-      if (initialBalance > 0.0) {
-          this.balance = initialBalance;
-      }
-  }
+    public Account(String accountHolder, double initialBalance) {
+        this.accountHolder = accountHolder;
+        if (initialBalance > 0.0) {
+            this.balance = initialBalance;
+        }
+    }
 
-  public void setAccountHolder(String accountHolder) {
-      this.accountHolder = accountHolder;
-  }
+    public void setAccountHolder(String accountHolder) {
+        this.accountHolder = accountHolder;
+    }
 
-  public String getAccountHolder() {
-      return accountHolder;
-  }
+    public String getAccountHolder() {
+        return accountHolder;
+    }
 
-  public void deposit(double depositAmount) {
-      if (depositAmount > 0.0) {
-          balance += depositAmount;
-      }
-  }
+    public void deposit(double depositAmount) {
+        if (depositAmount > 0.0) {
+            balance += depositAmount;
+        }
+    }
 
-  public double getBalance() {
-      return balance;
-  }
+    public double getBalance() {
+        return balance;
+    }
 }
